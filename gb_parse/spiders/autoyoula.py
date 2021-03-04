9d6206452bf2bc176f5b5c50e3077b834f820513
sofa.shvaryova <sofa.shvaryova@gmail.com>
2021-03-04 18:14:37 +0300

    '4hw'

--- gb_parse/spiders/autoyoula.py
+++ gb_parse/spiders/autoyoula.py
@@ -1,3 +1,5 @@
+import re
+import pymongo
 import scrapy
 
 
@@ -12,9 +14,7 @@ class AutoyoulaSpider(scrapy.Spider):
                   "a.blackLink",
         "pagination": "a.Paginator_button__u1e7D",
         "car": ".SerpSnippet_titleWrapper__38bZM a.SerpSnippet_name__3F7Yu",
-
     }
-
 
     def _get_follow(self, response, select_str, callback, **kwargs):
         for a in response.css(select_str):
@@ -26,6 +26,7 @@ class AutoyoulaSpider(scrapy.Spider):
                                     self._css_selectors["brands"],
                                     self.brand_parse,
                                     hello="moto")
+
     def brand_parse(self, response, **kwargs):
         yield from self._get_follow(response,
                                     self._css_selectors["pagination"],
@@ -35,16 +36,57 @@ class AutoyoulaSpider(scrapy.Spider):
                                     self._css_selectors["car"],
                                     self.car_parse
                                     )
-        print(1)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.db_client = pymongo.MongoClient()
+
+    @staticmethod
+    def get_author_id(resp):
+        marker = "window.transitState = decodeURIComponent"
+        for script in resp.css("script"):
+            try:
+                if marker in script.css("::text").extract_first():
+                    re_pattern = re.compile(r"youlaId%22%2C%22([a-zA-Z|\d]+)%22%2C%22avatar")
+                    result = re.findall(re_pattern, script.css("::text").extract_first())
+                    return resp.urljoin(f"/user/{result[0]}") if result else None
+            except TypeError:
+                pass
+
+    @staticmethod
+    def get_characteristics(response):
+        characteristics_list = []
+        for element in response.css("div.AdvertCard_specs__2FEHc .AdvertSpecs_row__ljPcX"):
+            line_info = {
+                "name": element.css(".AdvertSpecs_label__2JHnS::text").extract_first(),
+                "value": element.css(".AdvertSpecs_data__xK2Qx::text").extract_first(),
+            }
+            characteristics_list.append(line_info)
+        return characteristics_list
 
     def car_parse(self, response):
-        print(1)
-        # data = { }
-        # for key, selector in self.data_query.items():
-        #     try:
-        #         data[key] = selector(response)
-        #     except (ValueError, AttributeError):
-        #         continue
-        # self.db_client["gb_parse_15_02_2021"][self.name].insert_one(data)
-        #
+        """
+            # Собрать след стуркутру и сохранить в БД Монго
+            # Название объявления+
+            # Список фото объявления (ссылки)+
+            # Список характеристик
+            # Описание объявления
+            # ссылка на автора объявления
+            # дополнительно попробуйте вытащить телефона
+        """
+        items = {
+            "title": response.css("div.AdvertCard_advertTitle__1S1Ak::text").extract_first().strip(),
+            "picture": [img.attrib.get("src") for img in response.css("figure.PhotoGallery_photo__36e_r img")],
+            "characteristics": AutoyoulaSpider.get_characteristics(response),
+            "author": AutoyoulaSpider.get_author_id(response),
+            "price": int(("").join(response.css("div.AdvertCard_price__3dDCr::text").get().split("\u2009"))),
+        }
+        data = {}
+        for key, css_selector in items.items():
+            try:
+                data[key] = css_selector(response)
+            except Exception as err:
+                #print(Exception, err)
+                continue
+        self.db_client["gb_parse"][self.name].insert_one(data)
+        c = 1
