7f5103b96180da62464f3b42f3b3db0d95368e4f
sofa.shvaryova <sofa.shvaryova@gmail.com>
2021-03-09 01:24:21 +0300

    '5les'

--- gb_parse/spiders/hhparse.py
+++ gb_parse/spiders/hhparse.py
@@ -18,16 +18,27 @@ class HhparseSpider(scrapy.Spider):
         for link in vacancies_list:
             yield response.follow(link, callback=self.vacancy_parse)
 
+    # author_info_xpaths = {
+    #     "employer_name": "//span//text",
+    #     "employer_website": '//a[@class="g-user-content"]/@href',
+    #     "employer_areas": '//div[@class="bloko-text-emphasis"]//text()',
+    #     "employer_description": '//div[@class="company-description"]//text()',
+    # }
+
     def vacancy_parse(self, response, *args, **kwargs):
-        vacancy_title = response.css('h1.bloko-header-1::text').extract_first()
+        vacancy_title = response.xpath('//h1[@class="bloko-header-1"]/text()').extract_first()
         salary = ''.join(response.css('span.bloko-header-2::text').extract())
+        # ''.join(response.xpath('//p/span[@class="bloko-header-2 bloko-header-2_lite"]/text()').extract())
         description = response.xpath("//div[@class='vacancy-section']//text()").extract()
         key_skills = response.xpath("//span[@class='bloko-tag__section bloko-tag__section_text']/text()").extract()
         author = response.xpath("//a[@class='vacancy-company-name']/@href").get()
+        author_page = f'{"https://hh.ru/"}{author}'
+
         yield HhparseItem(vacancy_title=vacancy_title,
                           salary=salary,
                           description=description,
                           key_skills=key_skills,
                           author=author,
-                          # author_info=author_info
                           )
+
+
